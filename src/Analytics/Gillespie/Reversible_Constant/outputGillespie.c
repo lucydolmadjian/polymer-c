f1bb5ecadc1343a7cc99a223f1cf7a5c323ed423
Lara Clemens <lclemens@uci.edu>
2017-11-15 18:39:56 -0800

    include both fraction bound average and bound average in output

--- src/Analytics/Gillespie/Reversible_Constant/outputGillespie.c
+++ src/Analytics/Gillespie/Reversible_Constant/outputGillespie.c
@@ -6,15 +6,16 @@ void outputGillespie();
 //  GLOBAL VARIABLES for output control
 /*******************************************************************************/
 
-double finalBinaryState,averagePhosphorylationState;
-double sum_phosphorylationState,sum_time;
+double finalBinaryState,averagePhosphorylationFraction,averageBound;
+double sum_phosphorylationFraction,sum_time,sum_bound;
 int finalPhosphorylationState;
 
 /********************************************************************************************************/
 void outputGillespie()
 {
     // initializations
-    sum_phosphorylationState = 0;
+    sum_bound = 0;
+    sum_phosphorylationFraction = 0;
     sum_time = 0;
     
 
@@ -30,29 +31,33 @@ void outputGillespie()
     // find average phosphorylation state over last 100 states, taking time spent in state into account
     for(i=0;i<numberStatesStored;i++)
     {
-        sum_phosphorylationState += (double) timeStorage[i] * ((double) stateStorage[i] / (double) iSiteTotal);
-        sum_time                 += (double) timeStorage[i];
+        sum_bound                   += (double) timeStorage[i] * (double) stateStorage[i];
+        sum_phosphorylationFraction += (double) timeStorage[i] * ((double) stateStorage[i] / (double) iSiteTotal);
+        
+        sum_time                    += (double) timeStorage[i];
     }
-    averagePhosphorylationState = (double) sum_phosphorylationState/ (double) sum_time;
+    averageBound                    = (double) sum_bound / (double) sum_time;
+    averagePhosphorylationFraction  = (double) sum_phosphorylationFraction/ (double) sum_time;
     
     
     // look at final state
     finalPhosphorylationState = totalBound[finalState];
-    finalBinaryState = binaryState[finalState];
+    finalBinaryState          = binaryState[finalState];
     
     /***************** OUTPUT ********************/
 
     outputFile = fopen(outputName, "a");
 
     
-    fprintf(outputFile, "%e ", reverseRate);
-    fprintf(outputFile, "%d ", iSiteTotal);
-    fprintf(outputFile, "%f ", timeEnd);
-    fprintf(outputFile, "%f ", averagePhosphorylationState);
-    fprintf(outputFile, "%d ", finalState);
-    fprintf(outputFile, "%d ", finalPhosphorylationState);
-    fprintf(outputFile, "%f ", finalBinaryState);
-    fprintf(outputFile, "%f ", finalTotalTime);
+    fprintf(outputFile, "%e ", reverseRate);                        // 1
+    fprintf(outputFile, "%d ", iSiteTotal);                         // 2
+    fprintf(outputFile, "%f ", timeEnd);                            // 3
+    fprintf(outputFile, "%f ", averagePhosphorylationFraction);     // 4
+    fprintf(outputFile, "%f ", averageBound);                       // 5
+    fprintf(outputFile, "%d ", finalState);                         // 6
+    fprintf(outputFile, "%d ", finalPhosphorylationState);          // 7
+    fprintf(outputFile, "%f ", finalBinaryState);                   // 8
+    fprintf(outputFile, "%f ", finalTotalTime);                     // 9
     fprintf(outputFile, "\n");
 
     fclose(outputFile);
