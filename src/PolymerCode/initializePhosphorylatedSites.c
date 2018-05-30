326b3317ffff04c747e2f4ca5993cbf54321f901
Lara Clemens <lclemens@uci.edu>
2018-05-29 17:11:20 -0700

    update initializePhosphorylatedSites.c to have more comments, include error message if phosphorylation range extends past polymer end poins

--- src/PolymerCode/initializePhosphorylatedSites.c
+++ src/PolymerCode/initializePhosphorylatedSites.c
@@ -2,7 +2,6 @@
 
 void initializePhosphorylatedSites();
 
-
 /*******************************************************************************/
 //  GLOBAL VARIABLES for output control
 /*******************************************************************************/
@@ -10,81 +9,75 @@ void initializePhosphorylatedSites();
 int totalPhosphorylated;
 
 double phosiSites[NMAX];
-
-
-//
-/********************************************************************************************************/
+/*******************************************************************************/
 void initializePhosphorylatedSites()
 {
-        
-        
-        //initializes phoshorylatedSites to 0 (none phosphorylated)
-        for(ty=0;ty<iSiteTotal;ty++)
-        {
-            phosiSites[ty]=0;
-        }
-     
-        if (TALKATIVE) printf("This is a string: %s\n", occupiedSites);
-        
-        //read string and assign to double vector
-        
-        // 1 is occupied iSite (phosphorylated), 0 is unoccupied
-
-        //read string or read and interpret which of CD3Zeta Mouse tyrosines are phosphorylated
+    //initializes phoshorylatedSites to 0 (none phosphorylated)
+    for(ty=0;ty<iSiteTotal;ty++)
+    {
+        phosiSites[ty]=0;
+    }
+ 
+    if (TALKATIVE) printf("These are the occupied (phosphorylated) sites: %s\n", occupiedSites);
     
-        sscanf(occupiedSites,"%lf_%lf_%lf_%lf_%lf_%lf", &phosiSites[0],&phosiSites[1],&phosiSites[2],&phosiSites[3], &phosiSites[4],&phosiSites[5]);
+    //read string and assign to double vector
+    // 1 is occupied iSite (phosphorylated), 0 is unoccupied
+    //read string or read and interpret which of CD3Zeta Mouse tyrosines are phosphorylated
+    sscanf(occupiedSites,"%lf_%lf_%lf_%lf_%lf_%lf", &phosiSites[0],&phosiSites[1],&phosiSites[2],&phosiSites[3], &phosiSites[4],&phosiSites[5]);
 
-
-        //print which of the iSites are phosphorylated
-        if (TALKATIVE)
+    //print which of the iSites are phosphorylated
+    if (TALKATIVE)
+    {
+        for (iy=0;iy<iSiteTotal;iy++)
         {
-            for (iy=0;iy<iSiteTotal;iy++)
-            {
-                printf("phosiSites[ %ld ] =  %f\n",iy, phosiSites[iy]);
-				fflush(stdout);
-            }
-        }
-    
-        //initializes phosphorylated residues to 0 (none phosphorylated)
-        for(i=0;i<N;i++)
-        {
-            PhosphorylatedSites[i] =0;
-        }
-        
-        
-        
-        //Phosphorylate sites
-        for(ty=0;ty<iSiteTotal;ty++)
-        {
-            if(phosiSites[ty]==1) //might want to check the truth value on this - equals for double?
-            {
-                for(i=(iSite[ty]-PhosElectroRange);i<(iSite[ty]+PhosElectroRange+1);i++)
-                {
-                    PhosphorylatedSites[i]=1; //set that joint to "phosphorylated"
-                }
-            }
-        }
-    
-        if (TALKATIVE)
-        {
-            totalPhosphorylated = 0;
-            for (i=0;i<N;i++)
-            {
-                if (PhosphorylatedSites[i]==1)
-                {
-                    //printf("PhosphorylatedSites[ %ld ] =  %f\n",i, PhosphorylatedSites[i]);
-                    //fflush(stdout);
-                    totalPhosphorylated++;
-                }
-            }
-            printf("Total Phosphorylated: %d\n", totalPhosphorylated);
+            printf("phosiSites[ %ld ] =  %f\n",iy, phosiSites[iy]);
             fflush(stdout);
-
         }
-    
-    
+    }
 
+    //initializes phosphorylated residues to 0 (none phosphorylated)
+    for(i=0;i<N;i++)
+    {
+        PhosphorylatedSites[i] =0;
+    }
+
+    //Phosphorylate sites
+    // Note this is ONLY correct if PhosElectroRange does not extend past end points of polymer
+    for(ty=0;ty<iSiteTotal;ty++)
+    {
+        if(phosiSites[ty]==1) //might want to check the truth value on this - equals for double?
+        {
+            // include exit in case phosphorylation goes too far (can fix this to work appropriately if needed in the future)
+            if( (iSite[ty]-PhosElectroRange < 0) || (iSite[ty]+PhosElectroRange+1 > N) )
+            {
+                printf("Error! Phosphorylation extends beyond polymer!");
+                fflush(stdout);
+                exit(0);
+            }
+            // assign which segments are phosphorylated
+            for(i=(iSite[ty]-PhosElectroRange);i<(iSite[ty]+PhosElectroRange+1);i++)
+            {
+                PhosphorylatedSites[i]=1; //set that joint to "phosphorylated"
+            }
+        }
+    }
+
+    // for debugging, count and print total segments "phosphorylated"
+    if (TALKATIVE)
+    {
+        totalPhosphorylated = 0;
+        for (i=0;i<N;i++)
+        {
+            if (PhosphorylatedSites[i]==1)
+            {
+                //printf("PhosphorylatedSites[ %ld ] =  %f\n",i, PhosphorylatedSites[i]);
+                //fflush(stdout);
+                totalPhosphorylated++;
+            }
+        }
+        printf("Total Phosphorylated: %d\n", totalPhosphorylated);
+        fflush(stdout);
+    }
 }
-
-/********************************************************************************************************/
+/***********************************************************************************/
 
